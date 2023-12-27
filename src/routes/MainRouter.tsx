5bad549d9b649b309d3c224782e1fb3021683b67
Yamyam-code <rnffjt@gmail.com>
2023-12-27 17:23:15 +0900

    Conf: set absolute path

--- src/routes/MainRouter.tsx
+++ src/routes/MainRouter.tsx
@@ -1,7 +1,6 @@
+import Home from "@pages/Home/Home";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-
-import Home from "../pages/Home/Home";
 
 function MainRouter() {
   return (
