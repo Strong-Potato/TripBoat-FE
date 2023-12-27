77c04f4b1e30244aecb3bb5b4732177528fb76d2
Yamyam-code <rnffjt@gmail.com>
2023-12-27 17:23:15 +0900

    Conf: set absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,18 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [
+      { find: "@", replacement: "./src" },
+      { find: "@components", replacement: "./src/components" },
+      { find: "@assets", replacement: "./src/assets" },
+      { find: "@hooks", replacement: "./src/hooks" },
+      { find: "@pages", replacement: "./src/pages" },
+      { find: "@routes", replacement: "./src/routes" },
+      { find: "@types", replacement: "./src/types" },
+      { find: "@utils", replacement: "./src/utils" },
+    ],
+  },
   server: {
     watch: {
       usePolling: true,
