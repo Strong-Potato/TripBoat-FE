b1e0d405e0f3b5e4d99b09985385fae08423f10a
Yamyam-code <rnffjt@gmail.com>
2024-01-03 02:51:35 +0900

    Feat: voteathome markup

---
+++ src/components/Home/VoteAtHome/CardNull.tsx
@@ -0,0 +1,28 @@
+import { MdArrowForwardIos } from "react-icons/md";
+
+import styles from "./VoteAtHome.module.scss";
+
+function CardNull() {
+  return (
+    <div className={styles.voteBox}>
+      <div className={styles.contents}>
+        <div className={styles.left}>
+          <p className={styles.text}>
+            친구들과 함께
+            <br />
+            여행 장소를 정해보세요
+          </p>
+          <button>
+            <span>투표 만들기</span>
+            <p className={styles.arrow}>
+              <MdArrowForwardIos />
+            </p>
+          </button>
+        </div>
+        <img className={styles.right} src="/mapIconHome.png"></img>
+      </div>
+    </div>
+  );
+}
+
+export default CardNull;
