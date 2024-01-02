5c25fab2828e2bd5ac7851e2aea8f045083febfc
Yamyam-code <rnffjt@gmail.com>
2024-01-03 02:51:35 +0900

    Feat: voteathome markup

---
+++ src/components/Home/VoteAtHome/VoteAtHome.tsx
@@ -0,0 +1,29 @@
+import CardNull from "./CardNull";
+import CardHaveVote from "./CardHaveVote";
+import styles from "./VoteAtHome.module.scss";
+
+function VoteAtHome() {
+  const login = true;
+
+  return (
+    <div className={styles.container}>
+      {login ? (
+        <p className={styles.title}>
+          길동님,
+          <br />
+          진행 중인 투표가 있어요!
+        </p>
+      ) : (
+        <p className={styles.title}>
+          여행 계획 중이라면
+          <br />
+          트립보트에서 투표해볼까요?
+        </p>
+      )}
+
+      {login ? <CardHaveVote /> : <CardNull />}
+    </div>
+  );
+}
+
+export default VoteAtHome;
