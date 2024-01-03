8653207cd1eadf751f4c58f4edafd17fa2fac159
SKY-PEY <lovr8537@gmail.com>
2024-01-03 22:31:05 +0900

    Feat: add voteTitleModal markup

---
+++ src/pages/VoteDetail/VoteDetail.tsx
@@ -0,0 +1,12 @@
+import CreatVoteTitleModal from "@components/vote/CreatVoteTitleModal";
+
+const VoteDetail = () => {
+  return (
+    <div>
+      VoteDetail
+      <CreatVoteTitleModal />
+    </div>
+  );
+};
+
+export default VoteDetail;
