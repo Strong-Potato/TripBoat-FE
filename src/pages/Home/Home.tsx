781d0ac48e22336513f72cfcbd5a3771ada33767
Yamyam-code <rnffjt@gmail.com>
2024-01-04 10:59:12 +0900

    Fix: edit Home padding

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,28 @@
 import VoteAtHome from "@/components/Home/VoteAtHome/VoteAtHome";
 import styles from "./Home.module.scss";
 import RecommendedLocation from "@/components/Home/RecomendedItems/RecommendedLocation/RecommendedLocation";
-import RecomendedItem from "@/components/Home/RecomendedItems/Recomended/RecomendedItem";
+import RecomendedItem from "@/components/Home/RecomendedItems/RecommendedItem/RecommendedItem";
+import TripSpaceAtHome from "@/components/Home/TripSpaceAtHome/TripSpaceAtHome";
 
 function Home() {
+  const tripSpaceData = [
+    {
+      tripTitle: "아직 여행 일정이 없어요",
+      tripDay: "새로운 여행 일정을 만들어보세요!",
+      tripImg: "",
+    },
+    {
+      tripTitle: "아직 여행 일정이 없어요",
+      tripDay: "새로운 여행 일정을 만들어보세요!",
+      tripImg: "",
+    },
+    {
+      tripTitle: "아직 여행 일정이 없어요",
+      tripDay: "새로운 여행 일정을 만들어보세요!",
+      tripImg: "",
+    },
+  ];
+
   const recommendedLocation = ["제주", "부산", "강릉", "서울"];
 
   const recommendedData = [
@@ -30,29 +49,38 @@ function Home() {
   return (
     <div className={styles.container}>
       <VoteAtHome />
-      <p className={styles.title}>내 여행 일정</p>
-      <div>
-        <p className={styles.title}>지금 인기있는 여행지는?</p>
-        <div className={styles.recommended_location}>
-          {recommendedLocation.map((data, i) => (
-            <RecommendedLocation location={data} key={data + i} />
+      <div className={styles.section}>
+        <p className={styles.title}>내 여행 일정</p>
+        <div className={styles.item_list_8px}>
+          {tripSpaceData.map((data, i) => (
+            <TripSpaceAtHome data={data} key={data.tripTitle + i} />
           ))}
         </div>
       </div>
-      <div>
-        <p className={styles.title}>겨울에 어울리는 포근한 숙소</p>
-        <div className={styles.recommended_list}>
-          {recommendedData.map((data, i) => (
-            <RecomendedItem data={data} key={data.title + i} />
-          ))}
+      <div className={styles.lists_box}>
+        <div className={styles.section}>
+          <p className={styles.title}>지금 인기있는 여행지는?</p>
+          <div className={styles.item_list_8px}>
+            {recommendedLocation.map((data, i) => (
+              <RecommendedLocation location={data} key={data + i} />
+            ))}
+          </div>
         </div>
-      </div>
-      <div>
-        <p className={styles.title}>겨울을 신나게 즐기는 쌈박한 방법!</p>
-        <div className={styles.recommended_list}>
-          {recommendedData.map((data, i) => (
-            <RecomendedItem data={data} key={data.title + i} />
-          ))}
+        <div className={styles.section}>
+          <p className={styles.title}>겨울에 어울리는 포근한 숙소</p>
+          <div className={styles.item_list_16px}>
+            {recommendedData.map((data, i) => (
+              <RecomendedItem data={data} key={data.title + i} />
+            ))}
+          </div>
+        </div>
+        <div className={styles.section}>
+          <p className={styles.title}>겨울을 신나게 즐기는 쌈박한 방법!</p>
+          <div className={styles.item_list_16px}>
+            {recommendedData.map((data, i) => (
+              <RecomendedItem data={data} key={data.title + i} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
