4ecc3f48cdd5c33c186df0bb349663465fabe17c
SKY-PEY <lovr8537@gmail.com>
2024-01-04 15:44:11 +0900

    Feat: add voteHeader

--- src/pages/VoteDetail/VoteDetail.tsx
+++ src/pages/VoteDetail/VoteDetail.tsx
@@ -1,9 +1,13 @@
+import styles from "./VoteDetail.module.scss";
+
 import CreatVoteTitleModal from "@/components/vote/CreatVoteTitleModal";
+import VoteDetailsHeader from "@/components/vote/VoteDetailsHeader";
 
 const VoteDetail = () => {
   return (
-    <div>
-      VoteDetail
+    <div className={styles.container}>
+      <VoteDetailsHeader />
+
       <CreatVoteTitleModal />
     </div>
   );
