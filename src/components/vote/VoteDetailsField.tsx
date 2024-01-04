817ad04947b5730b9f63eda6837ee54dbe1659d9
SKY-PEY <lovr8537@gmail.com>
2024-01-04 18:14:33 +0900

    Design: add voteField and chakra custom theme

---
+++ src/components/vote/VoteDetailsField.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const VoteDetailsField = () => {
+  return (
+    <div>
+      <p>투표 후보가 없습니다.</p>
+      <p>하단 버튼을 눌러 후보를 추가해보세요.</p>
+    </div>
+  );
+};
+
+export default VoteDetailsField;
