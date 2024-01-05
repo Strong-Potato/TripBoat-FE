cab35532aa324f36df1dfd8f3d04d3f8131a885d
JinJeongMin <jdsjm2@naver.com>
2024-01-05 16:35:56 +0900

    fix: edit bottomBtn fixed & wrapper

--- src/pages/Detail/BottomFixedBtn/BottomFixedBtn.tsx
+++ src/pages/Detail/BottomFixedBtn/BottomFixedBtn.tsx
@@ -2,13 +2,15 @@ import styles from "./BottomFixedBtn.module.scss";
 
 function BottomFixedBtn() {
   return (
-    <div
-      className={styles.container}
-      style={{
-        backgroundColor: "#2388FF",
-      }}
-    >
-      <span>이 장소 후보로 등록하기</span>
+    <div className={styles.container}>
+      <div
+        className={styles.container__wrapper}
+        style={{
+          backgroundColor: "#2388FF",
+        }}
+      >
+        <span>이 장소 후보로 등록하기</span>
+      </div>
     </div>
   );
 }
