9c14fc27046b0ff2b3c538863f77ba168610c914
HOOOO98 <loading2us@gmail.com>
2024-01-05 19:25:05 +0900

    Feat: split sidebar component #4

--- src/routes/Dashboard/Dashboard.tsx
+++ src/routes/Dashboard/Dashboard.tsx
@@ -1,20 +1,10 @@
-import { useDisclosure } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
 import GlobalNavigationBar from "@/components/GlobalNavigationBar/GlobalNavigationBar";
-import SideBar from "@/components/SideBar/SideBar";
 
 function Dashboard() {
-  const {
-    onOpen: sideOpen,
-    isOpen: isSideOpen,
-    onClose: sideClose,
-  } = useDisclosure();
-
   return (
     <>
-      <button onClick={sideOpen}>Open</button>
-      <SideBar isSideOpen={isSideOpen} sideClose={sideClose} />
       <Outlet />
       <GlobalNavigationBar />
     </>
