d08a4280cb28cca8323cc8ddbd620ecdca03c045
SKY-PEY <lovr8537@gmail.com>
2024-01-06 03:54:13 +0900

    Design: markup the voteDetail page and add a buttonTheme in chakra

--- src/components/Vote/VoteDetailsHeader/VoteDetailsHeader.tsx
+++ src/components/Vote/VoteDetailsHeader/VoteDetailsHeader.tsx
@@ -7,12 +7,16 @@ import styles from "./VoteDetailsHeader.module.scss";
 const VoteDetailsHeader = () => {
   const voteTitle = "카페 어디로 갈래?";
 
+  //상태에 따른 아이콘 disabled
+  // 또는 없애기
+
   return (
     <div className={styles.container}>
-      <div className={styles.iconBox}>
+      <div className={styles.leftBackIcon}>
         <MdOutlineArrowBackIosNew />
-        <p>{voteTitle}</p>
       </div>
+      <p className={styles.title}>{voteTitle}</p>
+
       <div className={styles.iconBox}>
         <FaRegMap />
         <BsThreeDots />
--- src/pages/VoteDetail/VoteDetail.tsx
+++ src/pages/VoteDetail/VoteDetail.tsx
@@ -1,15 +1,19 @@
 import styles from "./VoteDetail.module.scss";
 
-import CreatVoteTitleModal from "@/components/Vote/CreatVoteTitleModal";
-import VoteDetailsField from "@/components/Vote/VoteDetailsField";
-import VoteDetailsHeader from "@/components/Vote/VoteDetailsHeader";
+// import CreatVoteTitleModal from "@/components/Vote/CreatVoteTitleModal/CreatVoteTitleModal";
+import VoteDetailsBottomButton from "@/components/Vote/VoteDetailsBottomButton/VoteDetailsBottomButton";
+import VoteDetailsField from "@/components/Vote/VoteDetailsField/VoteDetailsField";
+import VoteDetailsHeader from "@/components/Vote/VoteDetailsHeader/VoteDetailsHeader";
 
 const VoteDetail = () => {
   return (
     <div className={styles.container}>
       <VoteDetailsHeader />
       <VoteDetailsField />
-      <CreatVoteTitleModal />
+
+      {/* 나중에 여행스페이스 메인에 옮겨야함 */}
+      {/* <CreatVoteTitleModal /> */}
+      <VoteDetailsBottomButton />
     </div>
   );
 };
