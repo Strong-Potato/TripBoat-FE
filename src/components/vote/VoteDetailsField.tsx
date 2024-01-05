2427292ec523ac805205dfc97726aebc0c41d8fa
SKY-PEY <lovr8537@gmail.com>
2024-01-05 16:04:50 +0900

    Design: modifying css

--- src/components/vote/VoteDetailsField.tsx
+++ src/components/vote/VoteDetailsField.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import styles from "./VoteDetailsField.module.scss";
 
 const VoteDetailsField = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <p>투표 후보가 없습니다.</p>
       <p>하단 버튼을 눌러 후보를 추가해보세요.</p>
     </div>
--- src/pages/VoteDetail/VoteDetail.tsx
+++ src/pages/VoteDetail/VoteDetail.tsx
@@ -1,13 +1,14 @@
 import styles from "./VoteDetail.module.scss";
 
-import CreatVoteTitleModal from "@/components/vote/CreatVoteTitleModal";
-import VoteDetailsHeader from "@/components/vote/VoteDetailsHeader";
+import CreatVoteTitleModal from "@/components/Vote/CreatVoteTitleModal";
+import VoteDetailsField from "@/components/Vote/VoteDetailsField";
+import VoteDetailsHeader from "@/components/Vote/VoteDetailsHeader";
 
 const VoteDetail = () => {
   return (
     <div className={styles.container}>
       <VoteDetailsHeader />
-
+      <VoteDetailsField />
       <CreatVoteTitleModal />
     </div>
   );
