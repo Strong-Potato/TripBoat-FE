b5a6b785883ab7795ca2659d7e7d0db4eb18627f
NamgungJongMin <jmnamgung@gmail.com>
2024-01-07 18:09:17 +0900

    Feat: link router

--- src/routes/MainRouter/MainRouter.tsx
+++ src/routes/MainRouter/MainRouter.tsx
@@ -2,6 +2,8 @@ import { Route, Routes } from "react-router-dom";
 
 import Detail from "@/pages/Detail/Detail";
 import Home from "@/pages/Home/Home";
+import AgreePrivacy from "@/pages/Signup/Agree/AgreePrivacy";
+import AgreeService from "@/pages/Signup/Agree/AgreeService";
 import Dashboard from "@/routes/Dashboard/Dashboard";
 
 function MainRouter() {
@@ -14,6 +16,8 @@ function MainRouter() {
         <Route path="/user" element={<Home />} />
       </Route>
       <Route path="/detail/:id" element={<Detail />} />
+      <Route path="/signup/agreePrivacy" element={<AgreePrivacy />} />
+      <Route path="/signup/agreeService" element={<AgreeService />} />
     </Routes>
   );
 }
