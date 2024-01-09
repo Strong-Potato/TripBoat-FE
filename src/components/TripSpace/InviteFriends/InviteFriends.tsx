1fae9cfe1d8647e37fb4ea04108e30d7f0806886
HOOOO98 <loading2us@gmail.com>
2024-01-09 15:22:29 +0900

    Feat: implement kakao share button #49

--- src/components/TripSpace/InviteFriends/InviteFriends.tsx
+++ src/components/TripSpace/InviteFriends/InviteFriends.tsx
@@ -10,6 +10,7 @@ import InviteLogo from "@/assets/InviteLogo.svg?react";
 
 function InviteFriends() {
   const [isModalVisible, setIsModalVisible] = useState(false);
+
   const handleCopyClick = () => {
     //초대 링크 어떻게 넣을지 고민해보기
     navigator.clipboard.writeText("하하 반갑수다. 우리 같이 여행 갈마씸.");
@@ -17,6 +18,36 @@ function InviteFriends() {
     setTimeout(() => {
       setIsModalVisible(false);
     }, 1000);
+  };
+
+  const handleKakaoClick = () => {
+    window.Kakao.init(import.meta.env.VITE_KAKAO_KEY);
+    if (window.Kakao && window.Kakao.isInitialized()) {
+      window.Kakao.Share.createDefaultButton({
+        objectType: "feed",
+        container: "#kakaoShareButton",
+        content: {
+          title: "TRIPVOTE [강릉여행]",
+          description: "흰 돛과 일정만 있으면 어디든 갈 수 있어",
+          imageUrl:
+            "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FNaMMW%2FbtsC8Vt9uj3%2FqKZw6iL4TNxA8YR4IFTmXk%2Fimg.png",
+          link: {
+            mobileWebUrl: "http://localhost:5173",
+            webUrl: "http://localhost:5173",
+          },
+        },
+
+        buttons: [
+          {
+            title: "같이 여행하기",
+            link: {
+              mobileWebUrl: "http://localhost:5173",
+              webUrl: "http://localhost:5173",
+            },
+          },
+        ],
+      });
+    }
   };
 
   return (
@@ -33,7 +64,7 @@ function InviteFriends() {
           </p>
         </div>
         <div className={styles.container__wrapperButton}>
-          <button>
+          <button onClick={handleKakaoClick} id="kakaoShareButton">
             <InviteKakao />
             <p className={styles.container__a11y}>카카오톡 초대</p>
           </button>
