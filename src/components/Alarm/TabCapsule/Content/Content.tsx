a2000e85fd4a0cb2fced750d9562347d0d154c97
HOOOO98 <loading2us@gmail.com>
2024-01-11 19:19:59 +0900

    Fix: remove scrollbar #70

--- src/components/Alarm/TabCapsule/Content/Content.tsx
+++ src/components/Alarm/TabCapsule/Content/Content.tsx
@@ -4,17 +4,17 @@ import { ContentProps } from "@/types/alarm";
 
 function Content({ contents }: ContentProps) {
   return (
-    <>
+    <div className={styles.page}>
       {contents.map((content, index) => (
-        <button key={index} className={styles.container}>
+        <div key={index} className={styles.container}>
           <img src={content.url} className={styles.container__thumbnail} />
           <div className={styles.container__wrapper}>
             <p className={styles.container__wrapper__title}>{content.title}</p>
             <p className={styles.container__wrapper__time}>{content.time}</p>
           </div>
-        </button>
+        </div>
       ))}
-    </>
+    </div>
   );
 }
 
