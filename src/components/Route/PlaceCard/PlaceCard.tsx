04c5e976b5fc2c4d409f01b7f504e91f9af72622
JSH99 <tjgus9966@naver.com>
2024-01-14 22:44:22 +0900

    Feat: create route inner content components

---
+++ src/components/Route/PlaceCard/PlaceCard.tsx
@@ -0,0 +1,18 @@
+import styles from "./PlaceCard.module.scss";
+
+import { PlaceCardProps } from "@/types/\broute";
+
+function PlaceCard({ index, name, category, address }: PlaceCardProps) {
+  return (
+    <div className={styles.cardContainer}>
+      <div className={styles.numberContainer}>{index}</div>
+      <div className={styles.placeContainer}>
+        <h1>{name}</h1>
+        <h2>{category}</h2>
+        <p>{address}</p>
+      </div>
+    </div>
+  );
+}
+
+export default PlaceCard;
