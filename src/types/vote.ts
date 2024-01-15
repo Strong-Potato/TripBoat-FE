41964cdcaf6641e8f0fb7e57cc4b5a3c04be3d63
SKY-PEY <lovr8537@gmail.com>
2024-01-16 04:44:16 +0900

    Conf: organize chakra and type file

--- src/types/vote.ts
+++ src/types/vote.ts
@@ -1,21 +1,5 @@
 import { ReactNode } from "react";
 
-export interface VoteBottomButtonProps {
-  onClick: () => void;
-  title: string;
-}
-
-export interface VoteContentProps {
-  onBottomSlideOpen: (content: ReactNode) => void;
-  data: VoteListData;
-  showResults: boolean;
-}
-
-export interface VoteHeaderProps {
-  onBottomSlideOpen: () => void;
-  title: string;
-}
-
 export interface CandidateData {
   name: string;
   imageURL: string;
@@ -37,6 +21,68 @@ export interface VoteListData {
   id: string;
 }
 
+////////////////스웨거//////////////////
+export interface SSCandidateData {
+  id: number;
+  placeId: number; //
+  placeName: string;
+  category: string;
+  tagline: string; //
+  amIVoted: boolean; //
+  //imageURL: string;
+  //location: string;
+  //voteUserId: string[];
+  //voteCounts: number;
+  //memo: string;
+}
+[];
+export interface SSVoteData {
+  id: string;
+  title: string;
+  ownerProfile: {
+    id: number;
+    nickName: string;
+    profile: string;
+  };
+  candidates: CandidateData[];
+  // state: string;
+  // voteUserId: string[];
+}
+
+export interface SSVoteCardData {
+  voteId: string;
+  title: string;
+  ownerProfile: {
+    id: number;
+    nickName: string;
+    profile: string;
+  };
+  votedMemberProfiles: [
+    {
+      id: 0; //props으로 내려줄게 아니라면 사진만 있어도 됨, 배열로
+      nickName: string;
+      profile: string;
+    },
+  ];
+}
+////////////////스웨거//////////////////
+
+export interface VoteBottomButtonProps {
+  onClick: () => void;
+  title: string;
+}
+
+export interface VoteContentProps {
+  onBottomSlideOpen: (content: ReactNode) => void;
+  data: VoteListData;
+  showResults: boolean;
+}
+
+export interface VoteHeaderProps {
+  onBottomSlideOpen: () => void;
+  title: string;
+}
+
 export interface CandidateCardProps {
   onBottomSlideOpen: (content: ReactNode) => void;
   candidate: CandidateData;
@@ -55,4 +101,11 @@ export interface AlertModalProps {
   actionButton: string;
   isSmallSize: boolean;
   onClickAction?: () => void;
+}
+
+export interface CandidateListProps {
+  candidates: CandidateData[];
+  onBottomSlideOpen: (content: ReactNode) => void;
+  showResults: boolean;
+  isCandidateSelecting: boolean;
 }
