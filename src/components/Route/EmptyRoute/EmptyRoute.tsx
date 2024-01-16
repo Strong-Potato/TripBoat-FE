004431014a737adcd75982f9c1aec05684db07b3
JSH99 <tjgus9966@naver.com>
2024-01-16 15:52:03 +0900

    Feat: create day route component

--- src/components/Route/EmptyRoute/EmptyRoute.tsx
+++ src/components/Route/EmptyRoute/EmptyRoute.tsx
@@ -5,7 +5,7 @@ function EmptyRoute() {
     <div className={styles.emptyRouteContainer}>
       <div className={styles.textContainer}>
         <h1>생성된 일정이 없습니다.</h1>
-        <p>날짜를 정하고 일정을 만들어보세요!</p>
+        <p>+를 눌러 일정을 만들어보세요!</p>
       </div>
     </div>
   );
