824bdbcea3d835a607d9cae6fe0db23e0ae49b64
JSH99 <tjgus9966@naver.com>
2024-01-16 16:21:37 +0900

    Feat: create day navigation bar component

---
+++ src/components/Route/DayNavigationBar/DayNavigationBar.tsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import styles from "./DayNavigationBar.module.scss";
+
+import { DayNavigationBarProps } from "@/types/route";
+
+function DayNavigationBar({ dateList }: DayNavigationBarProps) {
+  const [selectedDay, setSelectedDay] = useState(1);
+
+  // TODO: 클릭 시 해당 day로 스크롤 이동
+  const handleDayClick = (day: number) => {
+    setSelectedDay(day);
+  };
+
+  return (
+    <nav>
+      <div className={styles.dayButtonContainer}>
+        {dateList.map((_, index) => (
+          <button
+            key={index}
+            className={index + 1 === selectedDay ? styles.activeButton : ""}
+            onClick={() => handleDayClick(index + 1)}
+          >
+            Day {index + 1}
+          </button>
+        ))}
+      </div>
+      <div className={styles.wholeEditButtonContainer}>
+        <button className={styles.wholeEditButton}>편집</button>
+      </div>
+    </nav>
+  );
+}
+
+export default DayNavigationBar;
