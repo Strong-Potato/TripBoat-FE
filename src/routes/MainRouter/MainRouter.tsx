b39da457ecf684d594918429c8dee2008ce315fb
JSH99 <tjgus9966@naver.com>
2024-01-19 23:19:36 +0900

    Fix: update route path'

--- src/routes/MainRouter/MainRouter.tsx
+++ src/routes/MainRouter/MainRouter.tsx
@@ -1,21 +1,21 @@
 import {Route, Routes} from 'react-router-dom';
 
-import AddPlaceFromVote from "@/pages/AddPlaceFromVote/AddPlaceFromVote";
-import FindPassword from "@/pages/Auth/FindPassword/FindPassword";
-import Login from "@/pages/Auth/Login/Login";
-import AgreePrivacy from "@/pages/Auth/Signup/Agree/AgreePrivacy";
-import AgreeService from "@/pages/Auth/Signup/Agree/AgreeService";
-import Signup from "@/pages/Auth/Signup/Signup";
-import Calendar from "@/pages/Calendar/Calendar";
+import AddPlaceFromVote from '@/pages/AddPlaceFromVote/AddPlaceFromVote';
+import FindPassword from '@/pages/Auth/FindPassword/FindPassword';
+import Login from '@/pages/Auth/Login/Login';
+import AgreePrivacy from '@/pages/Auth/Signup/Agree/AgreePrivacy';
+import AgreeService from '@/pages/Auth/Signup/Agree/AgreeService';
+import Signup from '@/pages/Auth/Signup/Signup';
+import Calendar from '@/pages/Calendar/Calendar';
 import CandidatesMap from '@/pages/CandidatesMap/CandidatesMap';
-import Detail from "@/pages/Detail/Detail";
-import Home from "@/pages/Home/Home";
-import RegionSearch from "@/pages/RegionSearch/RegionSearch";
-import SearchFromHome from "@/pages/SearchFromHome/SearchFromHome";
-import Trip from "@/pages/Trip/Trip";
-import Vote from "@/pages/Vote/Vote";
+import Detail from '@/pages/Detail/Detail';
+import Home from '@/pages/Home/Home';
+import RegionSearch from '@/pages/RegionSearch/RegionSearch';
+import SearchFromHome from '@/pages/SearchFromHome/SearchFromHome';
+import Trip from '@/pages/Trip/Trip';
+import Vote from '@/pages/Vote/Vote';
 import VoteMemo from '@/pages/Vote/VoteMemo/VoteMemo';
-import Dashboard from "@/routes/Dashboard/Dashboard";
+import Dashboard from '@/routes/Dashboard/Dashboard';
 
 function MainRouter() {
   return (
@@ -36,9 +36,9 @@ function MainRouter() {
       <Route path='/auth/signup/agreeService' element={<AgreeService />} />
       <Route path='/auth/findpassword' element={<FindPassword />} />
       <Route path='/home/search' element={<SearchFromHome />} />
-      <Route path='/trip/selectDate' element={<Calendar />} />
-      <Route path='/trip/selectRegion' element={<RegionSearch />} />
-      <Route path='/trip/add/vote' element={<AddPlaceFromVote />} />
+      <Route path='/trip/:id/selectDate' element={<Calendar />} />
+      <Route path='/trip/:id/selectRegion' element={<RegionSearch />} />
+      <Route path='/trip/:id/add/vote' element={<AddPlaceFromVote />} />
     </Routes>
   );
 }
