11b84cfe6d2d06bab6cf9628df760593f3406efd
JSH99 <tjgus9966@naver.com>
2024-01-20 00:26:47 +0900

    Feat: create map zoom in page

--- src/routes/MainRouter/MainRouter.tsx
+++ src/routes/MainRouter/MainRouter.tsx
@@ -10,6 +10,7 @@ import Calendar from '@/pages/Calendar/Calendar';
 import CandidatesMap from '@/pages/CandidatesMap/CandidatesMap';
 import Detail from '@/pages/Detail/Detail';
 import Home from '@/pages/Home/Home';
+import MapZoomIn from '@/pages/MapZoomIn/MapZoomIn';
 import RegionSearch from '@/pages/RegionSearch/RegionSearch';
 import SearchFromHome from '@/pages/SearchFromHome/SearchFromHome';
 import Trip from '@/pages/Trip/Trip';
@@ -38,6 +39,7 @@ function MainRouter() {
       <Route path='/home/search' element={<SearchFromHome />} />
       <Route path='/trip/:id/selectDate' element={<Calendar />} />
       <Route path='/trip/:id/selectRegion' element={<RegionSearch />} />
+      <Route path='/trip/:id/map' element={<MapZoomIn />} />
       <Route path='/trip/:id/add/vote' element={<AddPlaceFromVote />} />
     </Routes>
   );
