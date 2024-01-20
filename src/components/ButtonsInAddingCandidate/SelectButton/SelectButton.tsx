c56b4243eeb7f59dd23ecdd182d381fb1c0f2f63
SKY-PEY <lovr8537@gmail.com>
2024-01-21 06:07:19 +0900

    Feat: markup AddToCandidateButton

--- src/components/ButtonsInAddingCandidate/SelectButton/SelectButton.tsx
+++ src/components/ButtonsInAddingCandidate/SelectButton/SelectButton.tsx
@@ -2,11 +2,17 @@ import {useState} from 'react';
 
 import styles from './SelectButton.module.scss';
 
+import useGetSelectedCandidates from '@/hooks/useGetSelectedCandidates';
 const SelectButton = () => {
   const [isClicked, setIsClicked] = useState(false);
+  const {addCandidateInSelectedList} = useGetSelectedCandidates();
+
+  //"선택된 장소의 ID"
+  const placeId = 22;
 
   const handleClick = () => {
     setIsClicked((prev) => !prev);
+    addCandidateInSelectedList(placeId);
   };
 
   return (
