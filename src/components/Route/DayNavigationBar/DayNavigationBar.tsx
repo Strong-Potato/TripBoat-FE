b4c31cda32e7574604057f588535186a1878e159
JSH99 <tjgus9966@naver.com>
2024-01-21 04:11:09 +0900

    Hotfix: remove nav tag global style

--- src/components/Route/DayNavigationBar/DayNavigationBar.tsx
+++ src/components/Route/DayNavigationBar/DayNavigationBar.tsx
@@ -1,10 +1,10 @@
-import { useState } from "react";
+import {useState} from 'react';
 
-import styles from "./DayNavigationBar.module.scss";
+import styles from './DayNavigationBar.module.scss';
 
-import { DayNavigationBarProps } from "@/types/route";
+import {DayNavigationBarProps} from '@/types/route';
 
-function DayNavigationBar({ dateList }: DayNavigationBarProps) {
+function DayNavigationBar({dateList}: DayNavigationBarProps) {
   const [selectedDay, setSelectedDay] = useState(1);
 
   // TODO: 클릭 시 해당 day로 스크롤 이동
@@ -13,22 +13,24 @@ function DayNavigationBar({ dateList }: DayNavigationBarProps) {
   };
 
   return (
-    <nav>
-      <div className={styles.dayButtonContainer}>
-        {dateList.map((_, index) => (
-          <button
-            key={index}
-            className={index + 1 === selectedDay ? styles.activeButton : ""}
-            onClick={() => handleDayClick(index + 1)}
-          >
-            Day {index + 1}
-          </button>
-        ))}
-      </div>
-      <div className={styles.wholeEditButtonContainer}>
-        <button className={styles.wholeEditButton}>편집</button>
-      </div>
-    </nav>
+    <div className={styles.container}>
+      <nav>
+        <div className={styles.dayButtonContainer}>
+          {dateList.map((_, index) => (
+            <button
+              key={index}
+              className={index + 1 === selectedDay ? styles.activeButton : ''}
+              onClick={() => handleDayClick(index + 1)}
+            >
+              Day {index + 1}
+            </button>
+          ))}
+        </div>
+        <div className={styles.wholeEditButtonContainer}>
+          <button className={styles.wholeEditButton}>편집</button>
+        </div>
+      </nav>
+    </div>
   );
 }
 
