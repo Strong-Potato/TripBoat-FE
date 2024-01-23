97ac7b33efb0918f96711a07a867e66ffd09aab6
NamgungJongMin <jmnamgung@gmail.com>
2024-01-23 17:51:13 +0900

    Refactor: add redirect to login
    - modify profile page
    - user privacy page

--- src/pages/User/UserPrivacy/UserPrivacy.tsx
+++ src/pages/User/UserPrivacy/UserPrivacy.tsx
@@ -1,30 +1,44 @@
-import { Link } from "react-router-dom";
+import {Link, useNavigate} from 'react-router-dom';
 
-import styles from "./UserPrivacy.module.scss";
+import styles from './UserPrivacy.module.scss';
 
-import Header from "@/components/Auth/Header/Header";
+import {useGetMyInfo} from '@/hooks/User/useUser';
+
+import Header from '@/components/Auth/Header/Header';
 
 const userInfo = {
-  nickname: "개발토끼",
-  image: "https://avatars.githubusercontent.com/u/100336573?v=4",
-  provider: "KAKAO",
-  email: "test@gmail.com",
+  nickname: '개발토끼',
+  image: 'https://avatars.githubusercontent.com/u/100336573?v=4',
+  provider: 'KAKAO',
+  email: 'test@gmail.com',
 };
 
 function UserPrivacy() {
+  const navigate = useNavigate();
+  const {error, isFetching} = useGetMyInfo(true);
+
+  if (isFetching) {
+    return <div></div>;
+  }
+
+  if (error) navigate('/auth/login');
+
   return (
     <div className={styles.container}>
-      <Header content="계정 관리" />
+      <Header content='계정 관리' />
 
       <ul className={styles.userPrivacy}>
         <li className={styles.userPrivacy__email}>
           <div>트립보트 계정 이메일</div>
-          <small>{userInfo.email}</small>
+          <small>
+            {userInfo.email}
+            {/* data.email 로 수정 */}
+          </small>
         </li>
 
         <li className={styles.userPrivacy__password}>
           <div>비밀번호</div>
-          <Link to="/auth/password/modify">재설정</Link>
+          <Link to='/auth/password/modify'>재설정</Link>
         </li>
 
         <li
@@ -36,7 +50,7 @@ function UserPrivacy() {
         </li>
 
         <li>
-          <Link to="/auth/withdrawal">회원 탈퇴</Link>
+          <Link to='/auth/withdrawal'>회원 탈퇴</Link>
         </li>
       </ul>
     </div>
