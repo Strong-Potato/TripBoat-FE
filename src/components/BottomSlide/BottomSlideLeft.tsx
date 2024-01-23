725c575239d88d85b53f46170db84a8094986e49
JSH99 <tjgus9966@naver.com>
2024-01-23 15:00:59 +0900

    Feat: create day move component

---
+++ src/components/BottomSlide/BottomSlideLeft.tsx
@@ -0,0 +1,36 @@
+import {Slide} from '@chakra-ui/react';
+import {useRef} from 'react';
+
+import styles from './BottomSlide.module.scss';
+
+import useOnClickOutside from '@/hooks/useOnClickOutside';
+
+import CloseIcon from '@/assets/close.svg?react';
+
+import {BottomSlideProps} from '../../types/bottomSlide';
+
+function BottomSlideLeft({isOpen, onClose, children}: BottomSlideProps) {
+  const containerStyle = {
+    display: isOpen ? 'block' : 'none',
+  };
+
+  const slideRef = useRef(null);
+  useOnClickOutside(slideRef, onClose);
+
+  return (
+    <div className={styles.slideContainer} style={containerStyle}>
+      <Slide ref={slideRef} className={styles.slide} direction='bottom' in={isOpen}>
+        <div className={styles.slide__content}>
+          <div className={styles.leftCloseButtonContainer}>
+            <button onClick={onClose}>
+              <CloseIcon />
+            </button>
+          </div>
+          <div>{children}</div>
+        </div>
+      </Slide>
+    </div>
+  );
+}
+
+export default BottomSlideLeft;
