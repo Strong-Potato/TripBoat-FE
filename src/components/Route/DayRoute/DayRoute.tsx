2821a0b34a4ce51e0269a2f3692a48c8ae6695c8
HOOOO98 <loading2us@gmail.com>
2024-01-26 18:19:04 +0900

    Feat: add route optimizing function #186

--- src/components/Route/DayRoute/DayRoute.tsx
+++ src/components/Route/DayRoute/DayRoute.tsx
@@ -8,6 +8,8 @@ import styles from './DayRoute.module.scss';
 
 import BottomSlide from '@/components/BottomSlide/BottomSlide';
 
+import {findShortestPath} from '@/utils/optimizePlace';
+
 import AddPlace from '../AddPlace/AddPlace';
 import DraggablePlaceCard from '../DraggablePlaceCard/DraggablePlaceCard';
 import EmptyRoute from '../EmptyRoute/EmptyRoute';
@@ -17,6 +19,15 @@ import {DayRouteProps} from '@/types/route';
 function DayRoute({day, date, placeList, editMode, selectedPlaces, handlePlaceSelection}: DayRouteProps) {
   const {isOpen, onOpen, onClose} = useDisclosure();
   const [placeCards, setPlaceCards] = useState(placeList);
+  const [isOptimize, setIsOptimize] = useState(false);
+  const handleModalClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+  };
+
+  const handleBackgroundClick = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    setIsOptimize(true);
+  };
 
   const findCard = useCallback(
     (id: number) => {
@@ -63,7 +74,9 @@ function DayRoute({day, date, placeList, editMode, selectedPlaces, handlePlaceSe
           </button>
         </header>
         <div>
-          <button className={styles.optimizationButton}>루트 최적화</button>
+          <button className={styles.optimizationButton} onClick={() => setIsOptimize(true)}>
+            루트 최적화
+          </button>
           <div ref={drop} className={styles.placeListContainer}>
             {placeCards.length ? (
               placeCards.map((place) => (
@@ -88,6 +101,39 @@ function DayRoute({day, date, placeList, editMode, selectedPlaces, handlePlaceSe
         </div>
       </div>
       <BottomSlide isOpen={isOpen} onClose={onClose} children={<AddPlace />} />
+      {isOptimize && (
+        <div className={styles.background} onClick={handleBackgroundClick}>
+          <div className={styles.container} onClick={handleModalClick}>
+            <div className={styles.wrapperText}>
+              <p className={styles.wrapperText__title}>일정이 최적화 됩니다</p>
+              <p className={styles.wrapperText__body}>
+                일정 내 첫 번째 장소를 기준으로
+                <br />
+                N일차 일정이 최소 동선으로 재정렬 됩니다.
+              </p>
+            </div>
+            <div className={styles.wrapperButton}>
+              <button
+                className={styles.wrapperButton__cancel}
+                onClick={() => {
+                  setIsOptimize(false);
+                }}
+              >
+                취소
+              </button>
+              <button
+                className={styles.wrapperButton__accept}
+                onClick={() => {
+                  setPlaceCards(findShortestPath(placeList));
+                  setIsOptimize(false);
+                }}
+              >
+                확인
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
