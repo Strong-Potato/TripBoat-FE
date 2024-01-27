3ad25a7dcbe24ef7c61dced82d41b4d43a0ca6ed
JSH99 <tjgus9966@naver.com>
2024-01-28 02:44:10 +0900

    Feat: add navigate

--- src/components/Route/EmptyDate/EmptyDate.tsx
+++ src/components/Route/EmptyDate/EmptyDate.tsx
@@ -1,13 +1,18 @@
-import styles from "./EmptyDate.module.scss";
+import {useNavigate, useParams} from 'react-router-dom';
+
+import styles from './EmptyDate.module.scss';
 
 function EmptyDate() {
+  const {id} = useParams();
+  const navigate = useNavigate();
+
   return (
     <div className={styles.emptyDateContainer}>
       <div className={styles.textContainer}>
         <h1>생성된 일정이 없습니다.</h1>
         <p>날짜를 정하고 일정을 만들어보세요!</p>
       </div>
-      <button>날짜 정하러 가기</button>
+      <button onClick={() => navigate(`/trip/${id}/selectDate`, {state: {id: id}})}>날짜 정하러 가기</button>
     </div>
   );
 }
