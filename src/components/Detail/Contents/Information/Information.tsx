a22d304f2911d5912f183f3fc438093b50d0acac
JinJeongMin <jdsjm2@naver.com>
2024-01-27 19:51:05 +0900

    Feat: create link to Yanolja

--- src/components/Detail/Contents/Information/Information.tsx
+++ src/components/Detail/Contents/Information/Information.tsx
@@ -2,6 +2,10 @@ import {placeInfoDataPlace} from '@/types/detail';
 import BasicInformation from './BasicInformation/BasicInformation';
 // import Others from './Others/Others';
 import ShortReviews from './ShortReveiws/ShortReviews';
+import Yanolja from '@/assets/logo_yanolja.svg?react';
+import styles from './Information.module.scss';
+import {AiOutlineRight} from 'react-icons/ai';
+import {Link} from 'react-router-dom';
 
 export interface InformationProps {
   data: placeInfoDataPlace;
@@ -24,6 +28,18 @@ export interface InformationProps {
 function Information({data, onOpen, reviewsRating, reviews}: InformationProps) {
   return (
     <div>
+      <Link
+        className={styles.linkContainer}
+        to={`https://www.yanolja.com/search/${data.title}?keyword=${data.title}`}
+        target='_blank'
+      >
+        <Yanolja />
+        <div className={styles.linkContainer__text}>
+          <span className={styles.linkContainer__text__sale}>할인가</span>
+          <span className={styles.linkContainer__text__reserve}>예약하러 가기</span>
+          <AiOutlineRight fontSize='2rem' className={styles.linkContainer__text__icon} />
+        </div>
+      </Link>
       <BasicInformation
         location={data.location}
         openTime={data.openTime}
