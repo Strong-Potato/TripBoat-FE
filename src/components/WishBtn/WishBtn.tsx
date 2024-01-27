dfe6e947796dea40afe6ff00cce3acb5e851ae5b
JinJeongMin <jdsjm2@naver.com>
2024-01-27 09:12:27 +0900

    Feat: wish button not login modal

--- src/components/WishBtn/WishBtn.tsx
+++ src/components/WishBtn/WishBtn.tsx
@@ -1,13 +1,15 @@
 import {useEffect, useState} from 'react';
 import {FaHeart, FaRegHeart} from 'react-icons/fa';
-import {useSetRecoilState} from 'recoil';
 
 import {useDeleteWishes, useGetIsWish, usePostWishes} from '@/hooks/Detail/useWish';
 
-import {isModalOpenState, modalContentState} from '@/recoil/vote/alertModal';
-
 import CustomToast from '../CustomToast/CustomToast';
-import {useDebounce} from '@/hooks/useDebounce';
+import {useDebounceBoolean} from '@/hooks/useDebounce';
+import {Cookies} from 'react-cookie';
+
+import {Modal, ModalBody, ModalContent, ModalFooter, ModalOverlay} from '@chakra-ui/react';
+
+import styles from './WishBtn.module.scss';
 
 interface WishBtnProps {
   placeId: number;
@@ -16,48 +18,43 @@ interface WishBtnProps {
   className?: string;
 }
 
-const notLoginContent = {
-  title: '로그인이 필요한 기능입니다.',
-  subText: '로그인하고 모든 서비스를 이용해 보세요! ',
-  cancelText: '닫기',
-  actionButton: '로그인하기',
-  isSmallSize: true,
-};
-
 function WishBtn({placeId, contentTypeId, size = '2.4rem', className = ''}: WishBtnProps) {
-  const setIsModalOpen = useSetRecoilState(isModalOpenState);
-  const setModalContent = useSetRecoilState(modalContentState);
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [wishInitial, setWishInitial] = useState<boolean>(false);
 
-  // isLogin 구현해야 함
-  const isLogin = true;
+  const cookies = new Cookies();
+  const isLogin = cookies.get('isLogin');
 
   const showNotLoginModal = () => {
     setIsModalOpen(true);
-    setModalContent({...notLoginContent});
   };
 
   const showToast = CustomToast();
 
-  const {
-    data: {data: wish},
-  } = useGetIsWish(placeId);
-  const [isWish, setIsWish] = useState(wish);
+  const [isWish, setIsWish] = useState<boolean>(false);
+
+  if (isLogin) {
+    const {
+      data: {data: wish},
+    } = useGetIsWish(placeId);
+
+    setIsWish(wish);
+  }
+
   const postWishes = usePostWishes();
   const deleteWishes = useDeleteWishes();
 
-  // postWishes error 리턴 시 로그인 모달 띄우기
-  const debounce = useDebounce(isWish, 1000);
+  const debounce = useDebounceBoolean(isWish, 1000);
 
   const handleWishClick = () => {
     if (isLogin) {
       if (!isWish) {
-        // postWishes.mutate({placeId: placeId, contentTypeId: contentTypeId});
+        postWishes.mutate({placeId: placeId, contentTypeId: contentTypeId});
 
         setIsWish(true);
         showToast('찜 목록에 저장되었습니다.');
       } else {
-        // deleteWishes.mutate(placeId);
+        deleteWishes.mutate(placeId);
 
         showToast('찜 목록에서 제거되었습니다.');
         setIsWish(false);
@@ -105,6 +102,24 @@ function WishBtn({placeId, contentTypeId, size = '2.4rem', className = ''}: Wish
           className={className}
         />
       )}
+      <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} variant='alertModal'>
+        <ModalOverlay />
+        <ModalContent style={{margin: '288px 48px auto'}}>
+          <ModalBody>
+            <p className={styles.title}>로그인이 필요한 기능입니다.</p>
+            <span className={styles.subText}>로그인하고 모든 서비스를 이용해 보세요!</span>
+          </ModalBody>
+
+          <ModalFooter>
+            <button onClick={() => setIsModalOpen(false)} className={styles.buttons__cancel}>
+              닫기
+            </button>
+            <button onClick={() => {}} className={styles.buttons__action}>
+              로그인하
+            </button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
