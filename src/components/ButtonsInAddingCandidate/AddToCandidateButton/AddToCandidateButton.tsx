4fe9bd95097dd67af5c2e1224cc8693fa37794e7
Yamyam-code <rnffjt@gmail.com>
2024-01-28 09:38:00 +0900

    Feat: trip connect wish

--- src/components/ButtonsInAddingCandidate/AddToCandidateButton/AddToCandidateButton.tsx
+++ src/components/ButtonsInAddingCandidate/AddToCandidateButton/AddToCandidateButton.tsx
@@ -1,13 +1,13 @@
 import {Button} from '@chakra-ui/react';
 import {useLocation, useNavigate} from 'react-router-dom';
-import {useRecoilValue} from 'recoil';
+import {useRecoilValue, useSetRecoilState} from 'recoil';
 
-import {selectedPlaceState} from '@/recoil/vote/selectPlace';
+import {usePostPlaces} from '@/hooks/Spaces/space';
+
+import {selectedPlaceState, selectedTripPlaceState} from '@/recoil/vote/selectPlace';
 
 //prop이나 params로 trip or vote 판별, onClick에 삼항연산자로 함수 넣기
 const AddToCandidateButton = () => {
-  const selectedPlaces = useRecoilValue(selectedPlaceState);
-  const counts = selectedPlaces.length;
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const spaceId = queryParams.get('placeID');
@@ -16,10 +16,28 @@ const AddToCandidateButton = () => {
   const voteIdArray = tripDateParam?.split(' ') as string[]; // 띄어쓰기를 기준으로 문자열 분할
   const isInVote = voteIdArray[0] === 'vote';
   const voteId = voteIdArray[1];
+  const selectedPlaces = useRecoilValue(selectedPlaceState);
+  const selectedTripPlaces = useRecoilValue(selectedTripPlaceState);
+  const setTripRecoil = useSetRecoilState(selectedTripPlaceState);
+  const counts = isInVote ? selectedPlaces.length : selectedTripPlaces.length;
+  const postPlaces = usePostPlaces();
+
+  const addPlaces = async () => {
+    await postPlaces.mutateAsync({
+      spaceId: Number(spaceId),
+      journeyId: Number(voteId),
+      placeIds: selectedTripPlaces,
+    });
+    navigate(`/trip/${spaceId}`, {state: {id: spaceId}});
+    setTripRecoil([]);
+  };
+
   const handleAddCandidates = () => {
     // 경로 추후 수정
     if (isInVote) {
       navigate(`/trip/${spaceId}/votes/${voteId}/votememo`, {replace: true});
+    } else {
+      addPlaces();
     }
   };
   return (
